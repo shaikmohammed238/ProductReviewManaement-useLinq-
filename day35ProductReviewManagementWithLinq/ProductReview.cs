669da054ecb46e5b4d8001699d6ca45c6523a769
shaikmohammed238 <shaikmohammedghoue238@gmail.com>
2022-02-04 17:37:50 +0530

    added master

---
+++ day35ProductReviewManagementWithLinq/ProductReview.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace day35ProductReviewManagementWithLinq
+{
+    public class ProductReviews
+    {
+        public int ProductID { get; set; }
+        public int UserID { get; set; }
+        public double Rating { get; set; }
+        public string Reviews { get; set; }
+        public bool isLike { get; set; }
+    }
+}
